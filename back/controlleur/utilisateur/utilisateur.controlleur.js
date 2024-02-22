ad46bc20cebdf6459c77c5492bab34d28271e433
Sarobid <mbolasaroubidy@gmail.com>
2024-02-22 15:00:10 +0100

    controlle Nouveau employe (Back)

--- back/controlleur/utilisateur/utilisateur.controlleur.js
+++ back/controlleur/utilisateur/utilisateur.controlleur.js
@@ -1,7 +1,24 @@
 var utilisateurServ = require("./utilisateur.service");
 var serv = require("./../../service/errorService");
-
+var authServ = require("./../auth/auth.service");
 module.exports = function (app) {
+
+    app.post("/utilisateur/employe",(req,res)=>{
+        authServ.chekAutorisation(["MANAGER"],req,res)
+        .then(util=>{
+            utilisateurServ.nouveauUtilisateur("EMPLOYE",req.body.personne,req.body.email)
+            .then(data => {
+                res.json(data);
+            }).catch(err => {
+                res.status(err.status || 400);
+                serv.analyseError(err).then(error=>{res.send(error)})
+            });
+        }).catch(err => {
+            res.status(err.status || 400);
+            serv.analyseError(err).then(error=>{res.send(error)})
+        });
+    })
+
     app.post("/utilisateur/client",(req,res)=>{
         utilisateurServ.nouveauUtilisateur("CLIENT",req.body.personne,req.body.email,req.body.url)
         .then(data => {
@@ -34,15 +51,7 @@ module.exports = function (app) {
         });
     })
 
-    app.post("/utilisateur/employe",(req,res)=>{
-        utilisateurServ.nouveauUtilisateur("EMPLOYE",req.body.personne,req.body.email)
-        .then(data => {
-            res.json(data);
-        }).catch(err => {
-            res.status(err.status || 400);
-            serv.analyseError(err).then(error=>{res.send(error)})
-        });
-    })
+    
 
     app.get("/utilisateur/:id",(req,res)=>{
         utilisateurServ.findById(req.params.id)
