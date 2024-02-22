684abaacc2f3989fefbe2083b2b2fd80ac87640d
Fandrama <fandramaandriamanarivo@gmail.com>
2024-02-22 09:02:35 +0300

    service (back)

--- back/controlleur/service/service.controller.js
+++ back/controlleur/service/service.controller.js
@@ -27,4 +27,39 @@ module.exports = function (app) {
             res.send({error: {message: err.message},});
         });
     });
+    app.post("/service/update",(req,res)=>{
+        data={
+            nom : req.body.nom,
+            prix : req.body.prix,
+            duree : req.body.duree,
+            comission : req.body.comission,
+            delete : req.body.delete
+        }
+        serviceServ.updateDelete(req.body.id,data)
+        .then(data => {
+            res.json(data);
+        }).catch(err => {
+            res.status(err.status || 400);
+            res.send({error: {message: err.message},});
+        });
+    });
+    app.post("/service/searchMulti",(req,res)=>{
+        data={
+            nom : req.body.nom,
+            minPrix : req.body.minPrix,
+            maxPrix : req.body.maxPrix,
+            minDuree : req.body.minDuree,
+            maxDuree : req.body.maxDuree,
+            minComission : req.body.minComission,
+            maxComission : req.body.maxComission,
+            delete : req.body.delete
+        }
+        serviceServ.multiCriteriaSearch(req.body.id,data)
+        .then(data => {
+            res.json(data);
+        }).catch(err => {
+            res.status(err.status || 400);
+            res.send({error: {message: err.message},});
+        });
+    })
 }
--- back/controlleur/service/service.service.js
+++ back/controlleur/service/service.service.js
@@ -29,7 +29,7 @@ async function updateDelete(id) {
             { new: true } 
         );
         if (!updateService) {
-            throw new Error('Service not found');
+            throw new Error('erreur du suppression');
         }
         return updateService;
     } catch (error) {
@@ -37,3 +37,85 @@ async function updateDelete(id) {
     }
 }
 exports.updateDelete = updateDelete;
+
+async function updateService(id,update) {
+    try {
+        const updateService = await Serv.findByIdAndUpdate(
+            id, 
+            update,
+            { new: true } 
+        );
+        if (!updateService) {
+            throw new Error('Erreur du modification du service');
+        }
+        return updateService;
+    } catch (error) {
+        throw error;
+    }
+}
+exports.updateService = updateService;
+
+async function findServiceById(id) {
+    try {
+        const service = await Serv.findById(id);
+        if (!service) {
+            throw new Error('Service non trouvé');
+        }
+        return service;
+    } catch (error) {
+        throw error;
+    }
+}
+exports.findServiceById = findServiceById;
+
+async function multiCriteriaSearch(criteria) {
+    try {
+        const datas = {};
+        if (criteria.nom && (criteria.nom != '' || criteria.nom != null)) {
+            datas.nom = criteria.nom;
+        }
+        // if (criteria.prix && (criteria.prix != '' || criteria.prix != null)) {
+        //     datas.prix = criteria.prix;
+        // }
+
+        if ((criteria.minPrix !== undefined || criteria.minPrix !== null) && (criteria.maxPrix !== undefined || criteria.maxPrix !== null)) {
+            datas.prix = { $gte: criteria.minPrix, $lte: criteria.maxPrix };
+        } else if (criteria.minPrix !== undefined || criteria.minPrix !== null) {
+            datas.prix = { $gte: criteria.minPrix };
+        } else if (criteria.maxPrix !== undefined || criteria.maxPrix !== null) {
+            datas.prix = { $lte: criteria.maxPrix };
+        }
+
+        // if (criteria.duree && (criteria.duree != '' || criteria.duree != null)) {
+        //     datas.duree = criteria.duree;
+        // }
+        if ((criteria.minDuree !== undefined || criteria.minDuree !== null) && (criteria.maxDuree !== undefined || criteria.maxDuree !== null)) {
+            datas.duree = { $gte: criteria.minDuree, $lte: criteria.maxDuree };
+        } else if (criteria.minDuree !== undefined || criteria.minDuree !== null) {
+            datas.duree = { $gte: criteria.minDuree };
+        } else if (criteria.maxDuree !== undefined || criteria.maxDuree !== null) {
+            datas.duree = { $lte: criteria.maxDuree };
+        }
+        if (criteria.comission && (criteria.comission != '' || criteria.comission != null)) {
+            datas.comission = criteria.comission;
+        }
+
+        if ((criteria.minComission !== undefined || criteria.minComission !== null) && (criteria.maxComission !== undefined || criteria.maxComission !== null)) {
+            datas.comission = { $gte: criteria.minComission, $lte: criteria.maxComission };
+        } else if (criteria.minComission !== undefined || criteria.minComission !== null) {
+            datas.comission = { $gte: criteria.minComission };
+        } else if (criteria.maxComission !== undefined || criteria.maxComission !== null) {
+            datas.comission = { $lte: criteria.maxComission };
+        }
+
+        if (criteria.delete && (criteria.delete != '' || criteria.delete != null) ) {
+            datas.delete = criteria.delete;
+        }
+
+        const services = await Serv.find(datas);
+        return services;
+    } catch (error) {
+        throw error;
+    }
+}
+exports.multiCriteriaSearch = multiCriteriaSearch;
