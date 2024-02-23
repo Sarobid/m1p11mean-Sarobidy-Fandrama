8a2368a1b87803e7bc21be5cf8ab2e3e95206c09
Sarobid <mbolasaroubidy@gmail.com>
2024-02-23 12:06:09 +0100

    loading-router ( Front )

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavigationStart, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [NgIf,RouterOutlet, RouterLink, RouterLinkActive,LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front';
+  title = 'MEAN Beauty';
+  loading = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      } else {
+        this.loading = false;
+      }
+    });
+  }
 }
